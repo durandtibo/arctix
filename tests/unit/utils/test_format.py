eadd97b572d15e968e0c522a6bda9844ed928337
Thibaut Durand <durand.tibo@gmail.com>
2023-07-09 14:12:05 -0700

    Add `str_mapping`

--- tests/unit/utils/test_format.py
+++ tests/unit/utils/test_format.py
@@ -1,7 +1,34 @@
 from __future__ import annotations
 
+from arctix.utils.format import str_indent, str_mapping
 
-from arctix.utils.format import str_indent
+#################################
+#     Tests for str_mapping     #
+#################################
+
+
+def test_str_mapping_empty() -> None:
+    assert str_mapping({}) == ""
+
+
+def test_str_mapping_1_item() -> None:
+    assert str_mapping({"key": "value"}) == "(key): value"
+
+
+def test_str_mapping_2_items() -> None:
+    assert str_mapping({"key1": "value1", "key2": "value2"}) == "(key1): value1\n(key2): value2"
+
+
+def test_str_mapping_sorted_values_true() -> None:
+    assert (
+        str_mapping({"key2": "value2", "key1": "value1"}, sorted_keys=True)
+        == "(key1): value1\n(key2): value2"
+    )
+
+
+def test_str_mapping_sorted_values_false() -> None:
+    assert str_mapping({"key2": "value2", "key1": "value1"}) == "(key2): value2\n(key1): value1"
+
 
 ################################
 #     Tests for str_indent     #
