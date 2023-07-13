149450ebda483b4d7be775fe960952b4579af864
Thibaut Durand <durand.tibo@gmail.com>
2023-07-12 20:58:05 -0700

    Add tests and docstring to `summary`

--- src/arctix/summarizer.py
+++ src/arctix/summarizer.py
@@ -22,11 +22,47 @@ from arctix.formatter import (
 from arctix.utils.format import str_indent, str_mapping
 
 
-def summary(
-    value: Any,
-    max_depth: int = 1,
-    summarizer: BaseSummarizer | None = None,
-) -> str:
+def summary(value: Any, max_depth: int = 1, summarizer: BaseSummarizer | None = None) -> str:
+    r"""Summarize the input value in a string.
+
+    Args:
+    ----
+        value: Specifies the value to summarize.
+        max_depth (int, optional): Specifies the maximum depth to
+            summarize if the input is nested. Default: ``1``
+        summarizer (``BaseSummarizer`` or ``None``): Specifies the
+            summarization strategy. If ``None``, the default
+            ``Summarizer`` is used. Default: ``None``
+
+    Returns:
+    -------
+        str: The summary as a string.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from arctix import summary
+        >>> print(summary(1))
+        <class 'int'> 1
+        >>> print(summary(["abc", "def"]))
+        <class 'list'> (length=2)
+          (0): abc
+          (1): def
+        >>> print(summary([[0, 1, 2], {"key1": "abc", "key2": "def"}]))
+        <class 'list'> (length=2)
+          (0): [0, 1, 2]
+          (1): {'key1': 'abc', 'key2': 'def'}
+        >>> print(summary([[0, 1, 2], {"key1": "abc", "key2": "def"}], max_depth=2))
+        <class 'list'> (length=2)
+          (0): <class 'list'> (length=3)
+              (0): 0
+              (1): 1
+              (2): 2
+          (1): <class 'dict'> (length=2)
+              (key1): abc
+              (key2): def
+    """
     summarizer = summarizer or Summarizer()
     return summarizer.summary(value=value, depth=0, max_depth=max_depth)
 
@@ -38,7 +74,6 @@ class BaseSummarizer(ABC):
         value: Any,
         depth: int,
         max_depth: int,
-        summarizer: BaseSummarizer | None = None,
     ) -> str:
         pass
 
@@ -108,7 +143,6 @@ class Summarizer(BaseSummarizer):
         value: Any,
         depth: int,
         max_depth: int,
-        summarizer: BaseSummarizer | None = None,
     ) -> str:
         return self.find_formatter(type(value)).format(
             summarizer=self,
