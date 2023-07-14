a0cc9c46b3c0ba2e5b910a4aa3b1a4b3d91e077f
Thibaut Durand <durand.tibo@gmail.com>
2023-07-13 19:15:33 -0700

    Add `torch` dependency

---
+++ tests/unit/utils/test_imports.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from unittest.mock import patch
+
+from pytest import raises
+
+from arctix.utils.imports import check_torch, is_torch_available
+
+
+def test_check_torch_with_package() -> None:
+    with patch("arctix.utils.imports.is_torch_available", lambda *args: True):
+        check_torch()
+
+
+def test_check_torch_without_package() -> None:
+    with patch("arctix.utils.imports.is_torch_available", lambda *args: False):
+        with raises(RuntimeError, match="`torch` package is required but not installed."):
+            check_torch()
+
+
+def test_is_torch_available() -> None:
+    assert isinstance(is_torch_available(), bool)
