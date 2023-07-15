9480580b266819b940150abb770fbc27fe3ad096
Thibaut Durand <durand.tibo@gmail.com>
2023-07-15 10:25:49 -0700

    Add `numpy` dependency

--- src/arctix/testing.py
+++ src/arctix/testing.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-__all__ = ["torch_available"]
+__all__ = ["numpy_available", "torch_available"]
 
 from pytest import mark
 
-from arctix.utils.imports import is_torch_available
+from arctix.utils.imports import is_numpy_available, is_torch_available
 
+numpy_available = mark.skipif(not is_numpy_available(), reason="Requires NumPy")
 torch_available = mark.skipif(not is_torch_available(), reason="Requires PyTorch")
