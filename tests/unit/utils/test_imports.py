d854586070f46e1f64b4211c8fbde34231d96c37
Thibaut Durand <durand.tibo@gmail.com>
2023-07-15 10:41:20 -0700

    Add `numpy` dependency (#35)

--- tests/unit/utils/test_imports.py
+++ tests/unit/utils/test_imports.py
@@ -4,7 +4,27 @@ from unittest.mock import patch
 
 from pytest import raises
 
-from arctix.utils.imports import check_torch, is_torch_available
+from arctix.utils.imports import (
+    check_numpy,
+    check_torch,
+    is_numpy_available,
+    is_torch_available,
+)
+
+
+def test_check_numpy_with_package() -> None:
+    with patch("arctix.utils.imports.is_numpy_available", lambda *args: True):
+        check_numpy()
+
+
+def test_check_numpy_without_package() -> None:
+    with patch("arctix.utils.imports.is_numpy_available", lambda *args: False):
+        with raises(RuntimeError, match="`numpy` package is required but not installed."):
+            check_numpy()
+
+
+def test_is_numpy_available() -> None:
+    assert isinstance(is_numpy_available(), bool)
 
 
 def test_check_torch_with_package() -> None:
