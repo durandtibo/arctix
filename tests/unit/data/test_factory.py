ac6f5346c36162a308fdfc3d6d4b59db8320024e
Thibaut Durand <durand.tibo@gmail.com>
2023-07-17 19:24:00 -0700

    Add `setup_data_summary`

---
+++ tests/unit/data/test_factory.py
@@ -0,0 +1,24 @@
+from objectory import OBJECT_TARGET
+
+from arctix.data import NoOpDataSummary
+from arctix.data.factory import setup_data_summary
+
+########################################
+#     Tests for setup_data_summary     #
+########################################
+
+
+def test_setup_data_summary_object() -> None:
+    summary = NoOpDataSummary()
+    assert setup_data_summary(summary) is summary
+
+
+def test_setup_data_summary_dict() -> None:
+    assert isinstance(
+        setup_data_summary({OBJECT_TARGET: "arctix.data.NoOpDataSummary"}),
+        NoOpDataSummary,
+    )
+
+
+def test_setup_data_summary_none() -> None:
+    assert isinstance(setup_data_summary(None), NoOpDataSummary)
