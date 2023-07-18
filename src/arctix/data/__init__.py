c1006c44382fdefccb74480f0962ce86866bd637
Thibaut Durand <durand.tibo@gmail.com>
2023-07-17 18:18:52 -0700

    Add `BaseDataSummary`

---
+++ src/arctix/data/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["BaseDataSummary", "EmptyDataSummaryError", "NoOpDataSummary"]
+
+from arctix.data.base import BaseDataSummary, EmptyDataSummaryError
+from arctix.data.noop import NoOpDataSummary
---
+++ tests/unit/data/test_noop.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+import torch
+from pytest import mark
+
+from arctix.data import NoOpDataSummary
+
+#####################################
+#     Tests for NoOpDataSummary     #
+#####################################
+
+
+def test_noop_data_summary_str() -> None:
+    assert str(NoOpDataSummary()).startswith("NoOpDataSummary(")
+
+
+@mark.parametrize("data", (1, "abc", torch.ones(2, 3)))
+def test_noop_data_summary_add(data: Any) -> None:
+    summary = NoOpDataSummary()
+    summary.add(data)  # check it does not raise error
+
+
+def test_noop_data_summary_reset() -> None:
+    summary = NoOpDataSummary()
+    summary.add(1)
+    summary.reset()  # check it does not raise error
+
+
+def test_noop_data_summary_summary() -> None:
+    summary = NoOpDataSummary()
+    assert summary.summary() == {}
