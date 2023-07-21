c8fbce17b4eddf7460355efd4adf86b7bd7e4945
Thibaut Durand <durand.tibo@gmail.com>
2023-07-20 17:49:55 -0700

    Add `BaseTracker` and `ContinuousTracker`

---
+++ src/arctix/stats/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["BaseTracker", "EmptyTrackerError", "ContinuousTracker"]
+
+from arctix.stats.base import BaseTracker, EmptyTrackerError
+from arctix.stats.continuous import ContinuousTracker
