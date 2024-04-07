2a7dbc9c618287f41908f9a9a8c4221036267338
Thibaut Durand <durand.tibo@gmail.com>
2024-04-07 16:28:21 -0700

    Add `drop_duplicates` (#124)

---
+++ src/arctix/utils/dataframe/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain some utility functions to manipulate DataFrames."""
+
+from __future__ import annotations
+
+__all__ = ["drop_duplicates"]
+
+from arctix.utils.dataframe.removing import drop_duplicates
