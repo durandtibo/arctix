32c48938922286ff664c7e8a31d752e8ce3d72a4
Thibaut Durand <durand.tibo@gmail.com>
2024-04-08 18:28:14 -0700

    Add optional `tqdm` package

--- src/arctix/testing/__init__.py
+++ src/arctix/testing/__init__.py
@@ -2,6 +2,6 @@ r"""Contain some utility functions for testing."""
 
 from __future__ import annotations
 
-__all__ = ["gdown_available"]
+__all__ = ["gdown_available", "tqdm_available"]
 
-from arctix.testing.fixtures import gdown_available
+from arctix.testing.fixtures import gdown_available, tqdm_available
