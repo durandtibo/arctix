0f6a45736c4560ca0ca05c6b4d11e84f4b8d3769
Thibaut Durand <durand.tibo@gmail.com>
2024-04-10 22:48:56 -0700

    Add optional `matplotlib` package (#143)

--- src/arctix/utils/imports.py
+++ src/arctix/utils/imports.py
@@ -4,10 +4,13 @@ from __future__ import annotations
 
 __all__ = [
     "check_gdown",
+    "check_matplotlib",
     "check_tqdm",
     "gdown_available",
     "is_gdown_available",
+    "is_matplotlib_available",
     "is_tqdm_available",
+    "matplotlib_available",
     "tqdm_available",
 ]
 
@@ -91,6 +94,80 @@ def gdown_available(fn: Callable[..., Any]) -> Callable[..., Any]:
     ```
     """
     return decorator_package_available(fn, is_gdown_available)
+
+
+######################
+#     matplotlib     #
+######################
+
+
+def is_matplotlib_available() -> bool:
+    r"""Indicate if the ``matplotlib`` package is installed or not.
+
+    Returns:
+        ``True`` if ``matplotlib`` is available otherwise ``False``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arctix.utils.imports import is_matplotlib_available
+    >>> is_matplotlib_available()
+
+    ```
+    """
+    return find_spec("matplotlib") is not None
+
+
+def check_matplotlib() -> None:
+    r"""Check if the ``matplotlib`` package is installed.
+
+    Raises:
+        RuntimeError: if the ``matplotlib`` package is not installed.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arctix.utils.imports import check_matplotlib
+    >>> check_matplotlib()
+
+    ```
+    """
+    if not is_matplotlib_available():
+        msg = (
+            "`matplotlib` package is required but not installed. "
+            "You can install `matplotlib` package with the command:\n\n"
+            "pip install matplotlib\n"
+        )
+        raise RuntimeError(msg)
+
+
+def matplotlib_available(fn: Callable[..., Any]) -> Callable[..., Any]:
+    r"""Implement a decorator to execute a function only if
+    ``matplotlib`` package is installed.
+
+    Args:
+        fn: Specifies the function to execute.
+
+    Returns:
+        A wrapper around ``fn`` if ``matplotlib`` package is installed,
+            otherwise ``None``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from arctix.utils.imports import matplotlib_available
+    >>> @matplotlib_available
+    ... def my_function(n: int = 0) -> int:
+    ...     return 42 + n
+    ...
+    >>> my_function()
+
+    ```
+    """
+    return decorator_package_available(fn, is_matplotlib_available)
 
 
 ################
