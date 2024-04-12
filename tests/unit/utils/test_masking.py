8172831ca8ee20228379de04cb83e35343898a5d
Thibaut Durand <durand.tibo@gmail.com>
2024-04-11 23:11:46 -0700

    Add `convert_sequences_to_array` (#145)

--- tests/unit/utils/test_masking.py
+++ tests/unit/utils/test_masking.py
@@ -2,7 +2,47 @@ from __future__ import annotations
 
 import numpy as np
 
-from arctix.utils.masking import generate_mask_from_lengths
+from arctix.utils.masking import convert_sequences_to_array, generate_mask_from_lengths
+
+################################################
+#     Tests for convert_sequences_to_array     #
+################################################
+
+
+def test_convert_sequences_to_array() -> None:
+    assert np.array_equal(
+        convert_sequences_to_array([[1, 2, 3], [9, 8, 7, 6, 5], [1]], max_len=5),
+        np.array([[1, 2, 3, 0, 0], [9, 8, 7, 6, 5], [1, 0, 0, 0, 0]]),
+    )
+
+
+def test_convert_sequences_to_array_max_len_4() -> None:
+    assert np.array_equal(
+        convert_sequences_to_array([[1, 2, 3], [9, 8, 7, 6, 5], [1]], max_len=4),
+        np.array([[1, 2, 3, 0], [9, 8, 7, 6], [1, 0, 0, 0]]),
+    )
+
+
+def test_convert_sequences_to_array_max_len_6() -> None:
+    assert np.array_equal(
+        convert_sequences_to_array([[1, 2, 3], [9, 8, 7, 6, 5], [1]], max_len=6),
+        np.array([[1, 2, 3, 0, 0, 0], [9, 8, 7, 6, 5, 0], [1, 0, 0, 0, 0, 0]]),
+    )
+
+
+def test_convert_sequences_to_array_dtype() -> None:
+    assert np.array_equal(
+        convert_sequences_to_array([[1, 2, 3], [9, 8, 7, 6, 5], [1]], max_len=5, dtype=float),
+        np.array([[1.0, 2.0, 3.0, 0.0, 0.0], [9.0, 8.0, 7.0, 6.0, 5.0], [1.0, 0.0, 0.0, 0.0, 0.0]]),
+    )
+
+
+def test_convert_sequences_to_array_padded_value() -> None:
+    assert np.array_equal(
+        convert_sequences_to_array([[1, 2, 3], [9, 8, 7, 6, 5], [1]], max_len=5, padded_value=-1),
+        np.array([[1, 2, 3, -1, -1], [9, 8, 7, 6, 5], [1, -1, -1, -1, -1]]),
+    )
+
 
 ################################################
 #     Tests for generate_mask_from_lengths     #
