7af8feeb163fe5d86b26989d86e7a9e7bbb7f257
Thibaut Durand <durand.tibo@gmail.com>
2024-04-21 09:07:40 -0700

    Add multithumos dataset

---
+++ src/arctix/dataset/multithumos.py
@@ -0,0 +1,100 @@
+import logging
+import zipfile
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from iden.utils.path import sanitize_path
+
+from arctix.utils.download import download_url_to_file
+
+logger = logging.getLogger(__name__)
+
+ANNOTATION_URL = "http://ai.stanford.edu/~syyeung/resources/multithumos.zip"
+
+
+def download_data(path: Path, force_download: bool = False) -> None:
+    r"""Download the MultiTHUMOS annotation data.
+
+    Internally, this function downloads the annotations in a temporary
+    directory, then extracts the files from the download zip files in
+    the temporary directory, and finally moves the extracted files to
+    the given path.
+
+    Args:
+        path: The path where to store the MultiTHUMOS data.
+        force_download: If ``True``, the annotations are downloaded
+            everytime this function is called. If ``False``,
+            the annotations are downloaded only if the
+            given path does not contain the annotation data.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from pathlib import Path
+    >>> from arctix.dataset.multithumos import download_data
+    >>> path = Path("/path/to/data")
+    >>> download_data(path)  # doctest: +SKIP
+
+    ```
+    """
+    path = sanitize_path(path)
+    if not is_annotation_path_ready(path) or force_download:
+        with TemporaryDirectory() as tmpdir:
+            tmp_path = Path(tmpdir)
+            zip_file = tmp_path.joinpath("multithumos.zip.tmp")
+            logger.info(f"downloading MultiTHUMOS annotations data in {zip_file}...")
+            download_url_to_file(ANNOTATION_URL, zip_file.as_posix(), progress=True)
+
+            logger.info(f"extracting {zip_file} in {tmp_path}...")
+            with zipfile.ZipFile(zip_file, "r") as zip_ref:
+                zip_ref.extractall(tmp_path)
+
+            logger.info(f"moving extracted files to {path}...")
+            path.mkdir(parents=True, exist_ok=True)
+            tmp_path.joinpath("multithumos/README").rename(path.joinpath("README"))
+            tmp_path.joinpath("multithumos/class_list.txt").rename(path.joinpath("class_list.txt"))
+            tmp_path.joinpath("multithumos/annotations").rename(path.joinpath("annotations"))
+
+    logger.info(f"MultiTHUMOS annotation data are available in {path}")
+
+
+def is_annotation_path_ready(path: Path) -> bool:
+    r"""Indicate if the given path contains the MultiTHUMOS annotation
+    data.
+
+    Args:
+        path: The path to check.
+
+    Returns:
+        ``True`` if the path contains the MultiTHUMOS data,
+            otherwise ``False``.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from pathlib import Path
+    >>> from arctix.dataset.multithumos import is_annotation_path_ready
+    >>> is_annotation_path_ready(Path("/path/to/data/"))
+    False
+
+    ```
+    """
+    path = sanitize_path(path)
+    if not path.joinpath("README").is_file():
+        return False
+    if not path.joinpath("class_list.txt").is_file():
+        return False
+    if not path.joinpath("annotations").is_dir():
+        return False
+    return len(tuple(path.joinpath("annotations").glob("*.txt"))) == 65
+
+
+if __name__ == "__main__":  # pragma: no cover
+    import os
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    path = Path(os.environ["ARCTIX_DATA_PATH"])
+    download_data(path)
