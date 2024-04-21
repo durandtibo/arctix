6e97c9804d57dd753781d88bb0422ff8852b148b
Thibaut Durand <durand.tibo@gmail.com>
2024-04-21 13:00:55 -0700

    Add `multithumos.load_annotation_file` (#160)

--- tests/unit/dataset/test_multithumos.py
+++ tests/unit/dataset/test_multithumos.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from pathlib import Path
 from unittest.mock import Mock, patch
 from zipfile import ZipFile
 
@@ -13,11 +13,9 @@ from arctix.dataset.multithumos import (
     Column,
     download_data,
     is_annotation_path_ready,
+    load_annotation_file,
     parse_annotation_lines,
 )
-
-if TYPE_CHECKING:
-    from pathlib import Path
 
 
 @pytest.fixture(scope="module")
@@ -33,13 +31,17 @@ def data_zip_file(tmp_path_factory: pytest.TempPathFactory) -> Path:
 
 @pytest.fixture(scope="module")
 def data_file(tmp_path_factory: pytest.TempPathFactory) -> Path:
-    path = tmp_path_factory.mktemp("data").joinpath("action.txt")
+    path = tmp_path_factory.mktemp("data").joinpath("dribble.txt")
     save_text(
-        (
-            "video_validation_0000266 72.80 76.40  \n",
-            "video_validation_0000681 44.00 50.90 \n",
-            "video_validation_0000682 1.50 5.40\n",
-            "video_validation_0000682 79.30 83.90\n",
+        "\n".join(
+            [
+                "",
+                "  video_validation_0000266 72.80 76.40  ",
+                " video_validation_0000681 44.00 50.90 ",
+                "video_validation_0000682 1.50 5.40",
+                "   ",
+                "video_validation_0000682 79.30 83.90",
+            ]
         ),
         path,
     )
@@ -136,6 +138,33 @@ def test_is_annotation_path_ready_false_missing_annotation_file(tmp_path: Path) 
     for i in range(64):
         save_text("", tmp_path.joinpath(f"annotations/{i + 1}.txt"))
     assert not is_annotation_path_ready(tmp_path)
+
+
+##########################################
+#     Tests for load_annotation_file     #
+##########################################
+
+
+def test_load_annotation_file_incorrect_extension() -> None:
+    with pytest.raises(ValueError, match="Incorrect file extension."):
+        load_annotation_file(Mock(spec=Path))
+
+
+def test_load_annotation_file(data_file: Path) -> None:
+    assert objects_are_equal(
+        load_annotation_file(data_file),
+        {
+            Column.VIDEO: [
+                "video_validation_0000266",
+                "video_validation_0000681",
+                "video_validation_0000682",
+                "video_validation_0000682",
+            ],
+            Column.START_TIME: [72.80, 44.00, 1.50, 79.30],
+            Column.END_TIME: [76.40, 50.90, 5.40, 83.90],
+            Column.ACTION: ["dribble", "dribble", "dribble", "dribble"],
+        },
+    )
 
 
 ############################################
