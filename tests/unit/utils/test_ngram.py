0fe095685ea9d267822f79cac80229f2baa1b1e6
Thibaut Durand <durand.tibo@gmail.com>
2024-07-01 20:33:50 -0700

    Add `plot_ngrams` (#305)

--- tests/unit/utils/test_ngram.py
+++ tests/unit/utils/test_ngram.py
@@ -1,9 +1,23 @@
 from __future__ import annotations
 
+from collections import Counter
+
+import numpy as np
 import pytest
 from coola import objects_are_equal
 
-from arctix.utils.ngram import find_ngrams, find_seq_ngrams
+from arctix.utils.imports import is_matplotlib_available, matplotlib_available
+from arctix.utils.ngram import (
+    _create_transition_matrix,
+    find_ngrams,
+    find_seq_ngrams,
+    plot_ngrams,
+)
+from arctix.utils.vocab import Vocabulary
+
+if is_matplotlib_available():
+    import matplotlib.pyplot as plt
+
 
 #################################
 #     Tests for find_ngrams     #
@@ -80,3 +94,39 @@ def test_find_seq_ngrams_empty() -> None:
 def test_find_seq_ngrams_n_incorrect() -> None:
     with pytest.raises(RuntimeError, match="n must be greater or equal to 1"):
         find_seq_ngrams([["a", "b", "c", "d", "e"], ["f", "g", "h"], ["i"]], n=0)
+
+
+#################################
+#     Tests for plot_ngrams     #
+#################################
+
+
+@matplotlib_available
+def test_plot_ngrams() -> None:
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plot_ngrams(
+        ngrams=[("a", "b"), ("b", "c"), ("c", "d"), ("d", "e"), ("e", "f"), ("f", "g"), ("g", "h")],
+        ax=ax,
+    )
+
+
+@matplotlib_available
+def test_plot_ngrams_empty() -> None:
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plot_ngrams(ngrams=[], ax=ax)
+
+
+##############################################
+#     Tests for create_transition_matrix     #
+##############################################
+
+
+def test_create_transition_matrix() -> None:
+    assert objects_are_equal(
+        _create_transition_matrix(
+            counter=Counter([("a", "1"), ("b", "2"), ("c", "3"), ("e", "2")]),
+            previous_vocab=Vocabulary(Counter([("a",), ("b",), ("c",), ("d",), ("e",)])),
+            next_vocab=Vocabulary(Counter(["1", "2", "3"])),
+        ),
+        np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [0, 0, 0], [0, 1, 0]], dtype=np.float64),
+    )
